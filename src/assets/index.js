0a28445fa25caf73fbb2ef6b352d7f5a82b7a3e5
Eshan Srivastava <eshan,srivastava273@gmail.com>
2023-11-13 23:03:07 +0530

    started footer

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,11 +1,15 @@
 import testLogo from "./testLogo.svg";
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
+import logo from "./react.svg"
 import close from "./close.svg";
 import menu from "./menu.svg";
+import twitterlogo from "./twitter.svg"
+import linkedinlogo from "./linkedin.svg"
 
 export {
     testLogo,
     logo,
     close,
     menu,
+
 };
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,55 @@
+import { logo } from "../../assets/index"
+
+const Footer = () => {
+    return (
+        <div className="fixed bottom-0">
+            <div className="flex flex-row">
+                <div id="contact" className="flex flex-col">
+                    <img src={logo} alt="" />   
+                    <span>Medicine</span>
+                    <span>1429 Somethin Bridge, LA 4281</span>
+                    <span>Call: (321) 428 321 3902</span>
+                    <div>
+                        
+                    </div>
+                </div>
+                <div id="explore" className="flex flex-col">
+                    <h3>Explore</h3>
+                    <ul>
+                        <li>Feature</li>
+                        <li>About us</li>
+                        <li>FAQs</li>
+                        <li>Contact</li>
+                    </ul>
+                </div>
+                <div id="legal" className="flex flex-col">
+                    <h3>Legal</h3>
+                    <ul>
+                        <li>Privacy Policy</li>
+                        <li>Terms of Services</li>
+                        <li>Documentation</li>
+                        <li>Help center</li>
+                    </ul>
+                </div>
+                <div id="subscribe" className="flex flex-col">
+                    <h3>Subscribe</h3>
+                    <p>Subscribe to get the latest news from us</p>
+                    <input type="text" />
+                    <button>Subscribe</button>
+                </div>
+            </div>
+            <div>
+                <span>
+                    Copyright 2023 | Medical, All Rights Reserved
+                </span>
+                <div>
+                    <span>Privacy Policy</span>
+                    <span>Terms of services</span>
+                    <span>Accessibility</span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
