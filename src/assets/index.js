773aaf4de3ccaa0f6fde160286f9f01dc85e532a
Utkarsh Pandey <utk2665@gmail.com>
2023-11-14 20:48:40 +0530

    Update index

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,10 +1,6 @@
-import testLogo from "./testLogo.svg";
-import twitterlogo from "./twitter.svg"
-import linkedinlogo from "./linkedin.svg"
+
 import logo from './logo.png';
 
 export{
     logo,
-    twitterlogo,
-    linkedinlogo
 };
