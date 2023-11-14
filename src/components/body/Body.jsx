6326f5a6cf5566a6fa7d8c55061905e08050d6c7
Utkarsh Pandey <utk2665@gmail.com>
2023-11-15 03:13:00 +0530

    Body Section Added.

---
+++ src/components/body/Body.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './body.css'
+
+const Body = () => {
+  return (
+    <div className='hale__body flex flex-start sm:flex-col pl-[8rem] mt-[8rem]'>
+        <h1 className='font_heading-main flex flex-row text-5xl'>Experience Exceptional <br className='sm:block hidden'/>HealthCare In India <br className='sm:block hidden'/>With OmniCure.</h1>
+        <p className='mt-2 pt-6 ml-6'>Discover the pinnacle of healthcare services in the United States, where advancements, quality, cutting edge research, expert doctors and a commitment to patient success, combine to provide an unparalleled medical tourism experience. Omnicure USA is your dedicated partner, guiding you towards improved health and wellness. Connect with us today and embark on a journey of exceptional USA healthcare services.</p>
+    </div>
+  )
+}
+
+export default Body
