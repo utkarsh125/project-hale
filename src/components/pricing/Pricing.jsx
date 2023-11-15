b84cb897feaf95f3d6b55ccc1d93e80e967b8707
Utkarsh Pandey <utk2665@gmail.com>
2023-11-16 01:08:00 +0530

    Card Initialized

--- src/components/pricing/Pricing.jsx
+++ src/components/pricing/Pricing.jsx
@@ -4,7 +4,7 @@ import GlobalFamily from "./priceCards/GlobalFamily";
 
 const Pricing = () => {
   return (
-    <section className="bg-[#1E232F] pt-[8rem] pb-[8rem] mt-[2rem]">
+    <section className="bg-[#1E232F] pt-[8rem] pb-[8rem] mt-[2rem] bg-">
       <div className="flex flex-col w-[100%] justify-center">
         <h2 className="flex justify-center font-domine text-bold text-4xl text-[#FFF]">
           Global Plans
@@ -15,7 +15,7 @@ const Pricing = () => {
         </p>
       </div>
 
-      <div className="flex flex-row">
+      <div className="flex flex-row w-[100%] justify-center mt-[6rem]">
         <GlobalLite />
         <GlobalFamily />
       </div>
--- src/components/pricing/priceCards/GlobalLite.jsx
+++ src/components/pricing/priceCards/GlobalLite.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 const GlobalLite = () => {
   return (
-    <div>GlobalLite</div>
+    <div className='flex flex-col font-outfit text-[#fff]'>
+      <h3>Global Lite</h3>
+      <div className='flex flex-col'>
+        <p>Starting at</p>
+        <h2>₹1999</h2>
+      </div>
+      <div>
+        <ul>
+          <li></li>
+        </ul>
+      </div>
+    </div>
   )
 }
 
