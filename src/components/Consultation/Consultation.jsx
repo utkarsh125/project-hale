55c2354435d76d16f2e13d6819f5f1436a21b005
Utkarsh Pandey <utk2665@gmail.com>
2023-11-19 20:34:33 +0530

    Consultation.jsx added

--- src/components/Consultation/Consultation.jsx
+++ src/components/Consultation/Consultation.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import { formdoc } from '../../assets'
 
 const Consultation = () => {
   return (
     <div className='bg-gray-100 flex justify-center items-center mt-[4rem] pt-[8rem] pb-[8rem]'>
         <div className='w-[1237px] h-[884px]'>
-            <div className='bg-[#E1E1D6]' >
-                <h1 className='font-domine text-[#28574E] font-semibold text-4xl'>Book A Free Consultation</h1>
+            <div className='bg-[#E1E1D6] flex flex-row' >
+                <div className='flex flex-col'>
+                    <h1 className='font-domine text-[#28574E] font-semibold text-4xl'>Book A Free Consultation</h1>
+                    {/* FORM */}
+                    
+                </div>
+
+                <div className=''>
+                    <img src={formdoc} alt="" />
+                </div>
             </div>
         </div>
     </div>
