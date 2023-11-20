543a06870663530327f1ad451838518fd0348c0e
Eshan Srivastava <eshan,srivastava273@gmail.com>
2023-11-20 16:39:09 +0530

    fixed pricing and doctor responsiveness

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -15,10 +15,10 @@ const Header = () => {
             <button type='button' className='overflow-hidden w-1/4 px-2 font-normal sm:px-2 sm:py-2 md:py-3 md:px-4 ml-4 justify-center shadow-2xl rounded-full flex bg-[#FFFFFF] text-dark-bg-green sm:my-2.5 sm:w-[60%]'>Explore Premium Care</button>
           </div>
 
-          <div className='flex flex-row sm:block mx-auto sm:mx-5 md:mx-3 justify-center items-center'>
+          <div className='relative flex flex-row sm:block mx-auto sm:mx-5 md:mx-3 justify-center items-center'>
             <img src={doctor} alt="" className='mb-20 pt-10 z-[1] w-full'/>
             {/* Overlapping image */}
-            <img src={patient} alt="" className='w-1/5 rounded-lg right-36 top-28 z-[2] absolute'/>
+            <img src={patient} alt="" className='w-1/4 right-2 top-12 rounded-lg md:right-10 md:top-16 z-[2] absolute'/>
           </div>
         </div>        
     </div>
